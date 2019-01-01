04a5c27186997293832b6f4b685ffff9f9c35437
Tom Sullivan <tom@msbit.com.au>
2019-01-01 19:04:27 +1100

    Much better isochrone

--- index.js
+++ index.js
@@ -19,13 +19,72 @@ function makeResult(step) {
       }]
     }]
   };
-  return result; 
+  return result;
 }
 
-function normalise(inputMin, inputMax, outputMin, outputMax, input) {
-  const inputRange = inputMax - inputMin;
-  const outputRange = outputMax - outputMin;
-  return (((input - inputMin) / inputRange) * outputRange) + outputMin;
+function splitSteps(steps, duration) {
+  let result = [];
+  let currentDuration = 0;
+  let currentPoints = [];
+  for (let step of steps) {
+    const stepPoints = google.maps.geometry.encoding.decodePath(step.polyline.points);
+    const stepSpeed = step.distance.value / step.duration.value;
+
+    let prevPoint = stepPoints[0];
+    currentPoints.push({
+      point: stepPoints[0],
+      speed: stepSpeed
+    });
+    for (let i = 1; i < stepPoints.length; i++) {
+      const pointDistance = google.maps.geometry.spherical.computeDistanceBetween(prevPoint, stepPoints[i]);
+      currentDuration += pointDistance / stepSpeed;
+      currentPoints.push({
+        point: stepPoints[i],
+        speed: stepSpeed
+      });
+      if (currentDuration > result.length * duration) {
+        result.push(createStep(currentPoints));
+        currentPoints.length = 0;
+      }
+
+      prevPoint = stepPoints[i];
+    }
+  }
+  if (currentPoints.length) {
+    result.push(createStep(currentPoints));
+  }
+  return result;
+}
+
+function getDetails(data) {
+    let distance = 0;
+    let duration = 0;
+    let previous = data[0];
+
+    for (let i = 0; i < data.length; i++) {
+      const current = data[i];
+      const currentDistance = google.maps.geometry.spherical.computeDistanceBetween(previous.point, current.point);
+      distance += currentDistance;
+      duration += currentDistance / current.speed;
+      previous = data[i];
+    }
+    return [distance, duration];
+}
+
+function createStep(data) {
+  const [distance, duration] = getDetails(data);
+  return {
+    distance: {
+      text: distance.toString(),
+      value: distance
+    },
+    duration: {
+      text: duration.toString(),
+      value: duration
+    },
+    path: data.map(function (d) { return d.point; }),
+    travel_mode: google.maps.DirectionsTravelMode.DRIVING
+  };
 }
 
 function initMap() {
@@ -35,8 +94,8 @@ function initMap() {
     zoom: 8
   });
 
-  const start = 'new york';
-  const end = 'san francisco';
+  const start = '201 spring st, melbourne';
+  const end = '14 easey st, collingwood';
   const request = {
     origin: start,
     destination: end,
@@ -47,26 +106,20 @@ function initMap() {
     if (status == google.maps.DirectionsStatus.OK) {
       if (result.routes.length && result.routes[0].legs.length && result.routes[0].legs[0].steps.length) {
         map.fitBounds(result.routes[0].bounds);
-        const steps = result.routes[0].legs[0].steps;
-        const [minSpeed, maxSpeed] = steps.reduce(function (accumulator, step) {
-          const [min, max] = accumulator;
-          const speed = step.distance.value / step.duration.value;
-          return [speed < min ? speed : min, speed > max ? speed : max];
-        }, [0, 0]);
-        for (let step of steps) {
-          google.maps.geometry.encoding.encodePath(google.maps.geometry.encoding.decodePath(step.polyline.points));
-          const speed = step.distance.value / step.duration.value;
-          const speedNormalised = normalise(minSpeed, maxSpeed, 0, 120, speed);
+        const duration = result.routes[0].legs[0].duration.value;
+        const stepCount = 2;
+
+        splitSteps(result.routes[0].legs[0].steps, duration / stepCount).forEach(function (step, i) {
           const renderer = new google.maps.DirectionsRenderer({
             map: map,
             polylineOptions: {
-              strokeColor: `hsl(${speedNormalised}, 50%, 50%)`
+              strokeColor: `hsl(${120 - (i * (120 / stepCount))}, 50%, 50%)`
             },
             preserveViewport: true,
             suppressMarkers: true
           });
           renderer.setDirections(makeResult(step));
-        };
+        });
       }
     }
   });
